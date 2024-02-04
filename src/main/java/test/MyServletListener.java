9a8e9ac80035bc80e4e09af37a4f89bea20ccfdb
huangfeng <huangfeng@tongtech.com>
2024-02-04 16:36:59 +0800

    更新

--- src/main/java/test/MyServletListener.java
+++ src/main/java/test/MyServletListener.java
@@ -6,16 +6,16 @@ import javax.servlet.ServletContextListener;
 public class MyServletListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("========listener sleep.");
+       System.out.println("========listener Initialzed.");
         //long s=10*60*1000;
-        long s=100;
+       // long s=2*60*1000;
         //long s=1*60/10*1000;
-        try {
-            Thread.sleep(s);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("========listener initialized.");
+       // try {
+         //   Thread.sleep(s);
+      //  } catch (InterruptedException e) {
+        //    e.printStackTrace();
+      //  }
+      //  System.out.println("========listener initialized.");
     }
 
     @Override
